5a5253427da724bad1f61bef12b0b24cfba01876
Neil McCarthy <nmccarthy@yammer-inc.com>
2012-05-02 14:57:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Halfass::Application.config.secret_token = 'c1d1de330e5e93f579e8ef0012c036aae6b97a4501ca3630acdcc6772cca5092944254806b3ef2491871c30b1213cd8dbacbb117a9e1f8401a59158b03273610'
